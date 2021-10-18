d3f5ec62d97ecf9e325ab6f07b23ff7a2bf367e2
ITITIU20311 <ITITIU20311@student.hcmiu.edu.vn>
2021-10-18 17:21:38 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,6 +14,46 @@ Ex:
 
 void Ex3(int in_arr[], int n){
 	//Your codes here
+	if(n == 1){
+        printf("Increasing %d\nDecreasing %d", arr[0], arr[0]);
+        return;
+    }
+    int i;
+    int in_count = 1, de_count = 1;
+    int head = 0, tail = 0, in_head = 0, in_tail = 0, de_head = 0, de_tail = 0;
+    for(i = 0; i < n - 1; i++){
+        if(arr[i] <= arr[i + 1]){
+            tail = i + 1;
+        } else {
+            if(tail - head + 1 > in_count) {
+                in_count = tail - head + 1;
+                in_head = head;
+                in_tail = tail;
+            }
+            head = tail + 1;
+            tail = head;
+        }
+    }
+    head = 0, tail = 0;
+    for(i = 0; i < n - 1; i++){
+        if(arr[i] >= arr[i + 1]){
+            tail = i + 1;
+        } else {
+            if(tail - head + 1 > de_count) {
+                de_count = tail - head + 1;
+                de_head = head;
+                de_tail = tail;
+            }
+            head = tail + 1;
+            tail = head;
+        }
+    }
+    printf("Increasing ");
+    for(i = in_head; i <= in_tail; i++)
+        printf("%d ", arr[i]);
+    printf("Decreasing ");
+    for(i = de_head; i <= de_tail; i++)
+        printf("%d ", arr[i]);
 	
 }
 

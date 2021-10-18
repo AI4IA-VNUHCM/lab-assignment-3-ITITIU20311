ad61dabe607e774bb40bfa1d73c53195f838798d
ITITIU20311 <ITITIU20311@student.hcmiu.edu.vn>
2021-10-18 16:18:25 +0700

    Update Exercise5.c

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -13,6 +13,26 @@ ______________________________________
 
 void Ex5(int arr[], int n){
 	//Your codes here
+	int arrPo[n],arrNe[n],countPo,countNe;
+	countPo=0;
+	countNe=0;
+	for(int i=0 ; i<=n-1;i++)
+	{
+		if(arr[i]>=o)
+		{arrPo[countPo]=arr[i];
+		countPo++;}
+		else
+		{
+			arrNe[countNe]=arr[i];
+			countNe++;
+		}
+	}
+	for(int k=0;k<=countPo;k++) {
+		printf(" %d",arrPo[k]);
+	}
+	for(int k=0;k<=countNe;k++) {
+		printf(" %d",arrNe[k]);
+	}
 	
 }
 
